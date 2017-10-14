3941d011bbe18a139262c4fee86fb97f58d4a4ec
Curtis <cschwarzwahlfeld@gmail.com>
2017-10-14 23:46:19 +1100

    fixed syntax in entrypoint.sh again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#! /bin/bash/
+#!/bin/bash
 export parent_acc="$(cat /run/secrets/parent_acc)"
 export refresh_token="$(cat /run/secrets/refresh_token)"
 export rl_rate="$(cat /run/secrets/rl_rate)"
